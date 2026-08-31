ad8a7c6c12bbb6e89c0a55c73ab754c447c82160
HcmDZ <245678283+Hcmdz@users.noreply.github.com>
2026-08-31 02:09:18 +0100

    chore(version): bump to 6.3 (versionCode 28)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.HcmDz.ElecPilot"
         minSdk = 29
         targetSdk = 36
-        versionCode = 27
-        versionName = "6.2"
+        versionCode = 28
+        versionName = "6.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
         }
